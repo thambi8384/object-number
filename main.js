51478b604867e2c089b618e5967e260d1c79f02c
Your Name <nambimech01@gmail.com>
2022-12-22 10:41:13 +0530

    add

---
+++ main.js
@@ -0,0 +1,13 @@
+let phone={
+    name:"",
+    age:"",
+    bike:"",
+    color:""
+
+}
+console.log(phone);
+phone.name=prompt(("enter the name"));
+phone.age=prompt(("enter the age"));
+phone.bike=prompt(("enter the bike"));
+phone.color=prompt(("enter the color"));
+console.log(phone);
